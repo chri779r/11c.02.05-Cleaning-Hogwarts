6e90541348ebf857d662ddc8263139f8b45bea55
CW Munch <chri779r@stud.kea.dk>
2021-02-10 17:54:42 +0100

    name split working, trim() doesnt-2

--- script.js
+++ script.js
@@ -29,14 +29,12 @@ async function listStudents() {
     theStudents.forEach((student) => {
       const firstSpace = student.fullname.indexOf(" ");
       const lastSpace = student.fullname.indexOf(" ");
-      const fulleName = student.fullname;
       const firstName = student.fullname.substring(0, firstSpace).trim();
       const nickName = student.fullname.substring(firstSpace, lastSpace).trim();
       const middleName = student.fullname.substring(firstSpace, lastSpace).trim();
       const lastName = student.fullname.substring(lastSpace).trim();
       const firstLetter = student.fullname.substring(0, 1);
 
-      //const split = new Set([`student, fullname`]);
       const split = new Set([firstName, middleName, lastName]);
       let nameSplit = Array.from(split);
       console.log(nameSplit);
