488bab7d5e4821c8a02e9cf394abcac631bff3cd
CW Munch <chri779r@stud.kea.dk>
2021-02-10 14:48:51 +0100

    filter+studentJS

---
+++ script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", listStudents);
+
+async function listStudents() {
+  console.log("start");
+
+  let theStudents;
+  let theFilter = "all";
+  let popup = document.querySelector("#popup");
+
+  let url = "https://petlatkea.dk/2020/hogwarts/students.json";
+  let jsonData = await fetch(url);
+  theStudents = await jsonData.json();
+  console.log(theStudents);
+
+  document.querySelectorAll(".filter").forEach((button) => {
+    button.addEventListener("click", buttonClick);
+  });
+
+  showStudents();
+  function showStudents() {
+    console.log("showStudents");
+
+    let dest = document.querySelector("#students");
+    let temp = document.querySelector("#student_template");
+    dest.innerHTML = "";
+
+    theStudents.forEach((student) => {
+      if (theFilter == "all" || theFilter == student.house) {
+        //console.log(student);
+        let clone = temp.cloneNode(true).content;
+        clone.querySelector("h3").textContent = student.fullname;
+        clone.querySelector("#house").textContent = student.house;
+        clone.querySelector("#gender").textContent = student.gender;
+        clone.querySelector(".student").addEventListener("click", () => showDetails(student));
+        dest.appendChild(clone);
+      }
+    });
+  }
+
+  function showDetails(student) {
+    console.log(student);
+    popup.style.display = "block";
+    document.querySelector(".close").addEventListener("click", () => (popup.style.display = "none"));
+    popup.querySelector("h3").textContent = student.fullname;
+    popup.querySelector("#house").textContent = student.house;
+    popup.querySelector("#gender").textContent = student.gender;
+  }
+
+  function buttonClick() {
+    //console.log("buttonClick");
+    theFilter = this.dataset.student;
+    //console.log(theFilter);
+    document.querySelector(".section_header").textContent = this.textContent;
+    document.querySelectorAll(".filter").forEach((button) => {
+      button.classList.remove("clicked");
+    });
+    this.classList.add("clicked");
+    showStudents(theFilter);
+  }
+}
